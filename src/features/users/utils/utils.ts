7f9f308d79b7367870ca895a187fbd12ec53a011
Igor Vučko <ivucko1@fina.hr>
2025-04-09 08:28:25 +0200

    obrisan višak koda

--- src/features/users/utils/utils.ts
+++ src/features/users/utils/utils.ts
@@ -10,27 +10,3 @@ export const saveUsers = (users: User[]) => {
   localStorage.setItem("users", JSON.stringify(users));
 };
 
-export const plantUsers = () => {
-  if (!localStorage.getItem("users")) {
-    saveUsers([
-      {
-        id: 1,
-        firstName: "Pero",
-        lastName: "Perić",
-        address: "Bračka 12",
-        email: "ana@example.com",
-        phone: "0975435435",
-        oib: "10234512312",
-      },
-      {
-        id: 2,
-        firstName: "Savo",
-        lastName: "Savić",
-        address: "Vinkovačka 5",
-        email: "ivan@example.com",
-        phone: "092345513",
-        oib: "10986546537",
-      },
-    ]);
-  }
-};
